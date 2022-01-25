194cc54eab12621230bd693fef24bb8ccfd4cf0c
Zahid Vahabzade <83206656+Zahidoz@users.noreply.github.com>
2022-01-25 10:06:59 +0400

    first

--- src/App.js
+++ src/App.js
@@ -1,24 +1,55 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.scss';
+import {useState} from 'react'
 
 function App() {
+  const [inpt, setInpt] = useState('') 
+  const [todos,setTodos] = useState([])
+
+  const inputHandle = (e) => {
+    setInpt(e.target.value)
+  }
+  
+  const btnHandle = () => { 
+    if(inpt == ''){
+      alert('Input is Empty!')
+    }
+    else if(todos.find(todo => todo.todoName.toLowerCase() == inpt.toLowerCase())){
+      alert('This task is already have!')
+    }
+    else{
+      const newTodoName = {
+        todoName: inpt
+      }
+      setTodos([...todos,newTodoName])
+      setInpt('') 
+    }
+  }
+  const deleteHandle = (todoKey) => {
+    setTodos(todos.filter((todo,key)=> key != todoKey))
+  }
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+   <div className='container'>
+     <div className='searchSection'>
+        <div className='search'>            
+         <form onSubmit={(e) => e.preventDefault()}>
+         <input value={inpt} onChange={inputHandle} placeholder='Search here..'></input>
+          <button onClick={btnHandle}>Add</button>
+         </form>
+          </div>
+     </div>
+     <div className='taskSection'> 
+          <ul>
+            {todos.map((todo,key) =>(
+              <li key = {key}>{todo.todoName} 
+              <button className='btnDelete' onClick={() => deleteHandle(key)}>Delete</button>
+              </li>
+            ))
+            }
+          </ul>
+        </div>
+   </div>
   );
 }
 
